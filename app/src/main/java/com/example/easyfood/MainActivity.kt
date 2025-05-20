14601de91751a2898f68c11c6957154895444d9c
Mike Mambwe <mikeapple056@gmail.com>
2025-05-20 12:03:15 +0200

    app composable graph

--- app/src/main/java/com/example/easyfood/MainActivity.kt
+++ app/src/main/java/com/example/easyfood/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.easyfood
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
 import com.example.easyfood.ui.theme.EasyFoodTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,9 +13,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             EasyFoodTheme {
 
+                //entry point
+                AppEntryPoint()
                 }
 
         }
     }
+}
+
+@Composable
+fun AppEntryPoint() {
+    // This is where you can set up your navigation or main screen
+    // For example, you can call a function to set up the navigation graph
+    // NavigationGraph()
 }
 
---
+++ app/src/main/java/com/example/easyfood/ui/theme/components/AppNavigationGraph.kt
@@ -0,0 +1,8 @@
+package com.example.easyfood.ui.theme.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AppNavigationGraph(){
+
+}
