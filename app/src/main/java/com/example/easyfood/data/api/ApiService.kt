602ac8f6ad4e4e47293840a5935ba291e0fdd898
Mike Mambwe <mikeapple056@gmail.com>
2025-05-20 15:40:14 +0200

    the news response added to retrofit

--- app/src/main/java/com/example/easyfood/data/api/ApiService.kt
+++ app/src/main/java/com/example/easyfood/data/api/ApiService.kt
@@ -1,15 +1,18 @@
 package com.example.easyfood.data.api
 import com.example.easyfood.BuildConfig
+import com.example.easyfood.data.entity.NewsResponse
+import okhttp3.Response
+import retrofit2.http.GET
 
 import retrofit2.http.Query
 
 //
 interface ApiService {
-
+    @GET("v2/top-headlines")
     fun getNewsHeadlines(
         @Query("country") country: String,
         @Query("apiKey") apiKey: String = BuildConfig.NEWS_API_KEY,
-    )
+    ) : Response<NewsResponse>
 }
 
 //https://newsapi.org/v2/everything?q=bitcoin&apiKey=
